027889db435934c4fb6746afe334032669be1a2d
Chris Chang <ctchang001@gmail.com>
2024-04-04 21:33:45 -0400

    ORB_SLAM2 builds and runs properly now

--- setup.md
+++ setup.md
@@ -1,4 +1,82 @@
-pip install -r requirements.txt
+run bash setup.sh from project root
 
-git clone https://github.com/tharp789/Robo_Utils.git
+Some file changes are required for the build to function:
 
+1. Pangolin/CMakeModules/FindFFMPEG.cmake
+- line #63, 64
+FROM
+  sizeof(AVFormatContext::max_analyze_duration2);
+}" HAVE_FFMPEG_MAX_ANALYZE_DURATION2
+TO
+  sizeof(AVFormatContext::max_analyze_duration);
+}" HAVE_FFMPEG_MAX_ANALYZE_DURATION
+
+2. Pangolin/src/video/drivers/ffmpeg.cpp
+- line #37
+ADD
+#define CODEC_FLAG_GLOBAL_HEADER AV_CODEC_FLAG_GLOBAL_HEADER
+
+- line #78, 79
+FROM
+	TEST_PIX_FMT_RETURN(XVMC_MPEG2_MC);
+	TEST_PIX_FMT_RETURN(XVMC_MPEG2_IDCT);
+
+TO
+#ifdef FF_API_XVMC
+	TEST_PIX_FMT_RETURN(XVMC_MPEG2_MC);
+	TEST_PIX_FMT_RETURN(XVMC_MPEG2_IDCT);
+#endif
+
+- line #101-105
+FROM
+	TEST_PIX_FMT_RETURN(VDPAU_H264);
+	TEST_PIX_FMT_RETURN(VDPAU_MPEG1);
+	TEST_PIX_FMT_RETURN(VDPAU_MPEG2);
+	TEST_PIX_FMT_RETURN(VDPAU_WMV3);
+	TEST_PIX_FMT_RETURN(VDPAU_VC1);
+
+TO
+#ifdef FF_API_VDPAU
+	TEST_PIX_FMT_RETURN(VDPAU_H264);
+	TEST_PIX_FMT_RETURN(VDPAU_MPEG1);
+	TEST_PIX_FMT_RETURN(VDPAU_MPEG2);
+	TEST_PIX_FMT_RETURN(VDPAU_WMV3);
+	TEST_PIX_FMT_RETURN(VDPAU_VC1);
+#endif
+
+- line #127
+FROM
+	TEST_PIX_FMT_RETURN(VDPAU_MPEG4);
+
+TO
+#ifdef FF_API_VDPAU
+	TEST_PIX_FMT_RETURN(VDPAU_MPEG4);
+#endif
+
+3. Pangolin/include/pangolin/video/drivers/ffmpeg.h
+- line #53
+ADD
+#define AV_CODEC_FLAG_GLOBAL_HEADER (1 << 22)
+#define CODEC_FLAG_GLOBAL_HEADER AV_CODEC_FLAG_GLOBAL_HEADER
+#define AVFMT_RAWPICTURE 0x0020
+
+4. Pangolin/src/display/device/display_x11.cpp
+- line #115
+### THIS ONE BREAKS THE BUILD DO NOT USE ###
+FROM
+GLXFBConfig* fbc = glXChooseFBConfig(display, DefaultScreen(display), visual_attribs, &fbcount);
+
+TO
+// GLXFBConfig* fbc = glXChooseFBConfig(display, DefaultScreen(display), visual_attribs, &fbcount);
+cm
+
+- line #177
+FROM
+{
+	throw std::runtime_error("Pangolin in X11: ...");
+}
+
+TO
+{
+	// throw std::runtime_error("Pangolin in X11: ...");
+}
--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo apt update && sudo apt upgrade
+
+sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+    gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+    libtbb2 libtbb-dev libdc1394-dev
+
 # Create the dependencies folder if it doesn't exist
 mkdir -p dependencies
 
@@ -21,3 +29,62 @@ if [ ! -d "ORB_SLAM2" ]; then
 else
     echo "ORB_SLAM2 repository already exists locally."
 fi
+
+# Check if opencv directory exists
+if [ ! -d "opencv" ]; then
+    # Clone OpenCV repository
+    git clone https://github.com/opencv/opencv.git
+    cd opencv
+    git checkout 3.0.0
+    cd ..
+else
+    echo "OpenCV version 3.4.9 repository already exists locally."
+fi
+
+# Check if Pangolin directory exists
+if [ ! -d "Pangolin" ]; then
+    # Clone OpenCV repository
+    git clone --recursive https://github.com/stevenlovegrove/Pangolin.git
+    cd Pangolin
+    bash scripts/install_prerequisites.sh recommended
+    git checkout v0.5
+    cd ..
+else
+    echo "Pangolin version 0.5 repository already exists locally."
+fi
+
+# Make file changes
+cd ..
+rsync -av "file_changes/FindFFMPEG.cmake" "dependencies/Pangolin/CMakeModules/FindFFMPEG.cmake"
+rsync -av "file_changes/ffmpeg.cpp" "dependencies/Pangolin/src/video/drivers/ffmpeg.cpp"
+rsync -av "file_changes/ffmpeg.h" "dependencies/Pangolin/include/pangolin/video/drivers/ffmpeg.h"
+rsync -av "file_changes/display_x11.cpp" "dependencies/Pangolin/src/display/device/display_x11.cpp"
+rsync -av "file_changes/System.h" "dependencies/ORB_SLAM2/include/System.h"
+rsync -av "file_changes/LoopClosing.h" "dependencies/ORB_SLAM2/include/LoopClosing.h"
+cd dependencies
+
+
+
+# Make and install opencv
+cd opencv
+mkdir -p build && cd build
+cmake ..
+make -j16
+sudo make install
+cd ../..
+
+# Eigen3
+sudo apt install libeigen3-dev
+
+# Make and install Pangolin
+cd Pangolin
+cmake -B build
+cmake --build build
+
+cd build
+sudo make install
+cd ../..
+
+# ORB-SLAM2
+cd ORB_SLAM2
+bash build.sh
